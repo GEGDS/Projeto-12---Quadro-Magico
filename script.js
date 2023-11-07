3f197888fc84677b21358aab81c3ca8abed1ae00
Gabriel Silva <gabrielback2009@hotmail.com>
2023-11-06 23:04:55 -0300

    atualizando projeto

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const gridContainer = document.getElementById("grid-container");
+
+for (let i = 0; i < 16; i++) {
+  for (let j = 0; j < 16; j++) {
+    const cell = document.createElement("div"); 
+    cell.className = "grid-item"; // Adicione uma classe para estilização
+    gridContainer.appendChild(cell); // Adicione a célula ao grid container
+  }
+}
+
+
+//Footer
 const dataAtual = new Date();
 const ano = dataAtual.getFullYear();
 
